9385a8c1d751deb207947f9c8ae72b8c9a451d5b
Sebastian Stieber <stieber24@gmail.com>
2024-12-28 23:05:57 +0100

    try to fix temperature getting

--- drivers/TRVZB/device.js
+++ drivers/TRVZB/device.js
@@ -1,31 +1,36 @@
-'use strict';
+"use strict";
 
-const { ZigBeeDevice } = require('homey-zigbeedriver');
-const { Cluster, CLUSTER } = require('zigbee-clusters');
-const SonoffSpecificCluster = require('../../lib/SonoffSpecificCluster');
-const SonoffHelpers = require('../../lib/SonoffHelpers');
+const { ZigBeeDevice } = require("homey-zigbeedriver");
+const { Cluster, CLUSTER } = require("zigbee-clusters");
+const SonoffSpecificCluster = require("../../lib/SonoffSpecificCluster");
+const SonoffHelpers = require("../../lib/SonoffHelpers");
 
 Cluster.addCluster(SonoffSpecificCluster);
 
 class TRVZBDevice extends ZigBeeDevice {
-
   async onNodeInit({ zclNode }) {
-    this.log('Initializing TRVZB device');
+    this.log("Initializing TRVZB device");
+
+    // var localTemperatureRead =
+    //   await zclNode.endpoints[1].clusters.thermostat.readAttributes([
+    //     "localTemperature",
+    //   ]);
+    // console.log("localTemperatureRead", localTemperatureRead);
 
     // Set up attribute reporting
     await SonoffHelpers.configureReporting(this, [
       {
         endpointId: 1,
         cluster: CLUSTER.THERMOSTAT,
-        attributeName: 'localTemperature',
+        attributeName: "localTemperature",
         minInterval: 30,
         maxInterval: 300,
-        minChange: 10,
+        minChange: 1,
       },
       {
         endpointId: 1,
         cluster: CLUSTER.THERMOSTAT,
-        attributeName: 'occupiedHeatingSetpoint',
+        attributeName: "occupiedHeatingSetpoint",
         minInterval: 30,
         maxInterval: 300,
         minChange: 10,
@@ -33,71 +38,165 @@ class TRVZBDevice extends ZigBeeDevice {
     ]);
 
     // Register capabilities
-    this.registerCapability('measure_temperature', CLUSTER.THERMOSTAT, {
-      get: 'localTemperature',
-      report: 'localTemperature',
-      reportParser: value => value / 100,
+    // this.registerCapability("measure_temperature", zclNode.endpoints[1].clusters.thermostat, {
+    //   get: "localTemperature",
+    //   report: "localTemperature",
+    //   reportParser: (value) => value / 100,
+    //   getOpts: {
+    //     getOnStart: true,
+    //   },
+    // });
+
+    this.registerCapability("measure_temperature", CLUSTER.THERMOSTAT, {
+      report: "localTemperature",
+      reportParser: (value) => {
+        console.log("localTemperatureReportValue", value);
+        return value / 100;
+      },
+      get: "localTemperature",
+      getParser: (value) => {
+        console.log("localTemperatureGetValue", value);
+        return value / 100;
+      },
       getOpts: {
         getOnStart: true,
       },
     });
 
-    this.registerCapability('target_temperature', CLUSTER.THERMOSTAT, {
-      get: 'occupiedHeatingSetpoint',
-      report: 'occupiedHeatingSetpoint',
-      reportParser: value => value / 100,
+    this.registerCapability("target_temperature", CLUSTER.THERMOSTAT, {
+      get: "occupiedHeatingSetpoint",
+      report: "occupiedHeatingSetpoint",
+      reportParser: (value) => value / 100,
       getOpts: {
         getOnStart: true,
       },
     });
     this.registerCapabilityListener("target_temperature", async (value) => {
-			this.writeAttributes(CLUSTER.THERMOSTAT, {
-				occupiedHeatingSetpoint: value * 100
-			});
-		});
+      this.writeAttributes(CLUSTER.THERMOSTAT, {
+        occupiedHeatingSetpoint: value * 100,
+      });
+    });
 
     // Initial battery check
-    await SonoffHelpers.checkBattery(zclNode, this);
+    await this.checkBatteryHere(zclNode, this);
+
+    // Initial temperature check
+    await this.checkCurrentTemperature(zclNode, this);
 
     // Periodic battery check every 6 hours
     this.batteryCheckInterval = setInterval(() => {
-      SonoffHelpers.checkBattery(zclNode, this);
+      this.checkBatteryHere(zclNode, this);
     }, 6 * 60 * 60 * 1000); // Every 6 hours
+
+    // Periodic measure temperature every 15 min
+    this.temperatureCheckInterval = setInterval(() => {
+      this.checkCurrentTemperature(zclNode, this);
+    }, 15 * 60 * 1000); // Every 15 min
 
     // Read initial settings
     await this.readInitialSettings(zclNode);
 
     // Handle settings changes
     this.onSettings = this.onSettings.bind(this);
+
+    // temperatureMeasurement: TemperatureMeasurement
+    //  thermostat: ThermostatCluster
+  }
+
+  async checkBatteryHere(zclNode, device) {
+    device.log("Checking battery status");
+    try {
+      const value =
+        await zclNode.endpoints[1].clusters.powerConfiguration.readAttributes([
+          "batteryPercentageRemaining",
+        ]);
+      device.log("Battery value:", value);
+      if (value.batteryPercentageRemaining !== undefined) {
+        const batteryPercentage = value.batteryPercentageRemaining / 2;
+        await device.setCapabilityValue("measure_battery", batteryPercentage);
+      }
+    } catch (error) {
+      device.error("Failed to retrieve battery status", error);
+    }
+  }
+
+  async checkCurrentTemperature(zclNode, device) {
+    device.log("Checking current temperature");
+    try {
+      const value =
+        await zclNode.endpoints[1].clusters.thermostat.readAttributes([
+          "localTemperature",
+        ]);
+      device.log("Local temperature value:", value);
+      if (value.localTemperature !== undefined) {
+        const localTemperature = value.localTemperature / 100;
+        device.log('localTemperature:', localTemperature)
+        await device.setCapabilityValue(
+          "measure_temperature",
+          localTemperature
+        );
+      }
+    } catch (error) {
+      device.error("Failed to retrieve current temperature", error);
+    }
   }
 
   async readInitialSettings(zclNode) {
-    const settingsAttributes = ['child_lock', 'open_window', 'frost_protection_temperature'];
+    const settingsAttributes = [
+      "child_lock",
+      "open_window",
+      "frost_protection_temperature",
+    ];
     try {
-      const values = await SonoffHelpers.readAttributes(zclNode, SonoffSpecificCluster, settingsAttributes, this);
-      this.log('Initial settings read:', values);
+      // const values = await SonoffHelpers.readAttributes(
+      //   zclNode,
+      //   SonoffSpecificCluster,
+      //   settingsAttributes,
+      //   this
+      // );
+
+      const values =
+        await zclNode.endpoints[1].clusters.SonoffSpecificCluster.readAttributes(
+          settingsAttributes
+        );
+
+      this.log("Initial settings read:", values);
 
       const settings = {};
       for (const attr of settingsAttributes) {
-        settings[attr] = attr === 'frost_protection_temperature' ? values[attr] / 100 : values[attr];
+        settings[attr] =
+          attr === "frost_protection_temperature"
+            ? values[attr] / 100
+            : values[attr];
       }
       await this.setSettings(settings);
     } catch (error) {
-      this.error('Failed to read initial settings', error);
+      this.error("Failed to read initial settings", error);
     }
 
     // Read local temperature calibration
     try {
-      const { localTemperatureCalibration } = await SonoffHelpers.readAttributes(
-        zclNode,
-        CLUSTER.THERMOSTAT,
-        'localTemperatureCalibration',
-        this
+      const { localTemperatureCalibration } =
+        await zclNode.endpoints[1].clusters.thermostat.readAttributes([
+          "localTemperatureCalibration",
+        ]);
+
+      // await SonoffHelpers.readAttributes(
+      //   zclNode,
+      //   CLUSTER.THERMOSTAT,
+      //   "localTemperatureCalibration",
+      //   this
+      // );
+
+      this.log(
+        "Local temperature calibration read:",
+        localTemperatureCalibration
       );
-      this.log('Local temperature calibration read:', localTemperatureCalibration);
-      await this.setSettings({ localTemperatureCalibration: localTemperatureCalibration / 10 });
+      await this.setSettings({
+        localTemperatureCalibration: localTemperatureCalibration / 10,
+      });
     } catch (error) {
-      this.error('Failed to read local temperature calibration', error);
+      this.error("Failed to read local temperature calibration", error);
     }
   }
 
@@ -106,11 +205,13 @@ class TRVZBDevice extends ZigBeeDevice {
     const thermostatClusterAttributes = {};
 
     for (const key of changedKeys) {
-      if (key === 'localTemperatureCalibration') {
-        thermostatClusterAttributes.localTemperatureCalibration = Math.round(newSettings[key] * 10);
-      } else if (['child_lock', 'open_window'].includes(key)) {
+      if (key === "localTemperatureCalibration") {
+        thermostatClusterAttributes.localTemperatureCalibration = Math.round(
+          newSettings[key] * 10
+        );
+      } else if (["child_lock", "open_window"].includes(key)) {
         sonoffClusterAttributes[key] = newSettings[key];
-      } else if (key === 'frost_protection_temperature') {
+      } else if (key === "frost_protection_temperature") {
         sonoffClusterAttributes[key] = Math.round(newSettings[key] * 100);
       }
     }
@@ -118,48 +219,57 @@ class TRVZBDevice extends ZigBeeDevice {
     // Write to Sonoff-specific cluster
     if (Object.keys(sonoffClusterAttributes).length > 0) {
       try {
-        await SonoffHelpers.writeAttributes(this.zclNode, SonoffSpecificCluster, sonoffClusterAttributes, this);
-        this.log('Sonoff-specific settings updated:', sonoffClusterAttributes);
+        await SonoffHelpers.writeAttributes(
+          this.zclNode,
+          SonoffSpecificCluster,
+          sonoffClusterAttributes,
+          this
+        );
+        this.log("Sonoff-specific settings updated:", sonoffClusterAttributes);
       } catch (error) {
-        this.error('Failed to update Sonoff-specific settings', error);
+        this.error("Failed to update Sonoff-specific settings", error);
       }
     }
 
     // Write to thermostat cluster
     if (Object.keys(thermostatClusterAttributes).length > 0) {
       try {
-        await SonoffHelpers.writeAttributes(this.zclNode, CLUSTER.THERMOSTAT, thermostatClusterAttributes, this);
-        this.log('Thermostat settings updated:', thermostatClusterAttributes);
+        await SonoffHelpers.writeAttributes(
+          this.zclNode,
+          CLUSTER.THERMOSTAT,
+          thermostatClusterAttributes,
+          this
+        );
+        this.log("Thermostat settings updated:", thermostatClusterAttributes);
       } catch (error) {
-        this.error('Failed to update thermostat settings', error);
+        this.error("Failed to update thermostat settings", error);
       }
     }
   }
 
   onDeleted() {
-    this.log('TRVZB device removed');
+    this.log("TRVZB device removed");
     if (this.batteryCheckInterval) {
       clearInterval(this.batteryCheckInterval);
     }
   }
 
   async writeAttributes(cluster, attribs) {
-		if ("NAME" in cluster)
-		cluster = cluster.NAME;
-		try {
-			this.log("Write attribute", attribs);
-			this.zclNode.endpoints[1].clusters[cluster]
-				.writeAttributes(attribs) 
-				.then((value) => {			
-					this.log("Write attr", attribs);
-				})
-				.catch(() => {			
-					this.error("Error write attr", attribs);
-				});
-		} catch (error) {
-			this.error('Error (2) read', attribs, error);
-		}
-	}
+    if ("NAME" in cluster) cluster = cluster.NAME;
+    try {
+      this.log("Write attribute", attribs);
+      this.zclNode.endpoints[1].clusters[cluster]
+        .writeAttributes(attribs)
+        .then((value) => {
+          this.log("Write attr", attribs);
+        })
+        .catch(() => {
+          this.error("Error write attr", attribs);
+        });
+    } catch (error) {
+      this.error("Error (2) read", attribs, error);
+    }
+  }
 }
 
 module.exports = TRVZBDevice;
